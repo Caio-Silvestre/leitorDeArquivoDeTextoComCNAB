f7fb69039a45aefcb654af0922cf085f189afb5a
Caio S.G <caiosilvestre.dev@gmail.com>
2023-01-26 10:25:11 -0300

    Captura de dados do arquivo e tratamento

--- handleCnab/forms.py
+++ handleCnab/forms.py
@@ -1,12 +1,22 @@
-from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import UploadFileForm, handle_uploaded_file
+from rest_framework.views import APIView, Request, Response, status
+from rest_framework import generics
 from .serializer import FileSerializer
 from .models import HandleCnab
+import ipdb
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('success/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
 
-
-class UploadFileForm(forms.Form):
-    file = forms.FileField()
-
-
-
-def handle_uploaded_file(f):
+class successViews(APIView):
     ...
+
+    
